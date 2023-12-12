3b5ef5c3a9b394dd81045b7dc2f032e2394240bd
Rommel Olachea <msc.rommel.olachea@gmail.com>
2023-12-12 14:06:27 -0700

    Inicio de proyecto

---
+++ lib/presentation/screens/cubir_counter_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CubirCounterScreen extends StatelessWidget {
+  const CubirCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.refresh_outlined)),
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value: xxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag:
+                1, //cuando tenemos mas de un FLoatinActionButton, necesitamos indicarle a flutter cual es el que se anima entre scaffolds
+            child: const Text('+3'),
+            onPressed: () => {},
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: 2,
+            child: const Text('+2'),
+            onPressed: () => {},
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: 3,
+            child: const Text('+1'),
+            onPressed: () => {},
+          )
+        ],
+      ),
+    );
+  }
+}
